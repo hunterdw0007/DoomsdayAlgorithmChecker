c44020ab6ad3b0206021442ae16395ea789d9132
Hunter Westerlund <hunterw.mail@gmail.com>
2021-12-03 18:28:31 -0600

    Update Doomsday.py

--- Doomsday.py
+++ Doomsday.py
@@ -46,7 +46,7 @@ print("Doomsday Dates are as follows:\n\t1/3-4\t2/28-29\t  3/14\n\t4/4\t5/9\t  6
 
 print('Weekdays can be represented as numbers like so:\n\tSunday = 0\n\tMonday = 1\n\tTuesday = 2\n\tWednesday = 3\n\tThursday = 4\n\tFriday = 5\n\tSaturday = 6\n')
 
-print('Doomsday for 2000 is Tuesday (2) and Doomsday for 1900 is Wednesday (3)\n')
+print('Doomsday for 2000 is Tuesday (2), Doomsday for 1900 is Wednesday (3),\nDoomsday for 1800 is Friday (5), Doomsday for 1700 is Sunday (0)\nThis pattern repeats forwards and backwards for all centuries\n')
 
 print('Special years:\n\t12 = 1\t24 = 2\t36 = 3\t48 = 4\n\t60 = 5\t72 = 6\t84 = 7\t96 = 8\n')
 
